7f3ed203e368711a30e063d56f90081fb98b278b
dwatts <daniel.h.watts@gmail.com>
2025-12-18 17:45:31 -0700

    Updates to splash screen

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 /***Fade in Splash Screen on Load***/
 
+function showDivInner() {
+  $(".splash-inner").fadeIn(500);
+}
+
 $(document).ready(function(){
     $(".splash-container")
     .css("display", "flex")
     .hide()
     .fadeIn();
+    setTimeout(showDivInner, 2000);
 });
 
 /***Close Splash Screen***/
